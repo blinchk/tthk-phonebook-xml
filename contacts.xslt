71eda4bdf6dae44442e8a088942dc92c7ca4e46a
Nikolas Laus <bchik3@gmail.com>
2021-11-21 15:37:21 +0200

    Add stylesheet and basic parsing functions to PHP

--- contacts.xslt
+++ contacts.xslt
@@ -1,6 +1,6 @@
 <?xml version="1.0" encoding="utf-8" ?>
 <xsl:stylesheet xmlns:xsl="http://www.w3.org/1999/XSL/Transform" version="1.0">
-    <xsl:output method="xml" indent="yes">
-
-    </xsl:output>
+    <xsl:template match="/">
+        <h1>Contacts</h1>
+    </xsl:template>
 </xsl:stylesheet>
--- index.php
+++ index.php
@@ -5,9 +5,36 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <title>Contacts</title>
 </head>
 <body>
     <h1>Contacts</h1>
+    <?php
+    $file = "contacts.xml";
+    $contacts = simplexml_load_file($file) or die ('Cannot parse your XML');
+    ?>
+    <table id="contacts-table" border="1">
+        <tr>
+            <th>ID</th>
+            <th>First Name</th>
+            <th>Last Name</th>
+            <th>Phone</th>
+            <th>Email</th>
+            <th>Instagram</th>
+        </tr>
+    <?php
+    foreach($contacts as $contact) {
+        echo "<tr>";
+        echo "<td>$contact->id</td>";
+        echo "<td>$contact->firstname</td>";
+        echo "<td>$contact->lastname</td>";
+        echo "<td>$contact->phone</td>";
+        echo "<td>$contact->email</td>";
+        echo "<td>$contact->instagram</td>";
+        echo "</tr>";
+    }
+    ?>
+    </table>
+    <link rel="stylesheet" href="index.css">
 </body>
 </html>
