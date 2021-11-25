4a4d071e26183cb5148b34d5808afe5075f4dc49
Roasteg <krostislav468@gmail.com>
2021-11-25 08:53:01 +0200

    Correct download method

--- index.php
+++ index.php
@@ -5,8 +5,6 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link href="https://fonts.googleapis.com/icon?family=Material+Icons"
-          rel="stylesheet">
     <script src="script.js"></script>
     <title>Contacts</title>
 </head>
@@ -57,11 +55,11 @@ $contacts = simplexml_load_file($file) or die ('Cannot parse your XML');
             <label for="lName">Last name</label>
         </div>
         <div class="formInput">
-            <input type="text" name="phone">
+            <input type="text" name="phone" pattern="[0-9]+">
             <label for="phone">Phone</label>
         </div>
         <div class="formInput">
-            <input type="text" name="email">
+            <input type="email" name="email">
             <label for="email">Email</label>
         </div>
         <div class="formInput">
@@ -100,6 +98,13 @@ function convertToJSON($xml)
     $file_json = fopen($file, "w") or die ("Unable to open file!");
     fwrite($file_json, $json);
     fclose($file_json);
+    header('Content-type: application/octet-stream');
+    header("Content-Type: ".mime_content_type($file));
+    header("Content-Disposition: attachment; filename=".$file);
+    while (ob_get_level()) {
+        ob_end_clean();
+    }
+    readfile($file);
 }
 
 function sendDataToXml($xml)
